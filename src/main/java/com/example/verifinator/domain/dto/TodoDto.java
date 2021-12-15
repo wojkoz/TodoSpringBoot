7e6ebf44a4d27909b9747ae15c727d730b6803e3
Wojciech Kozioł <wkoziol3ib@gmail.com>
2021-12-15 12:45:06 +0100

    added models

---
+++ src/main/java/com/example/verifinator/domain/dto/TodoDto.java
@@ -0,0 +1,51 @@
+package com.example.verifinator.domain.dto;
+
+public class TodoDto {
+    private Long id;
+    private String title;
+    private String description;
+
+    public TodoDto(Long id, String title, String description){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public TodoDto(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "TodoDto{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/verifinator/domain/entity/Todo.java
@@ -0,0 +1,51 @@
+package com.example.verifinator.domain.entity;
+
+public class Todo {
+    private Long id;
+    private String title;
+    private String description;
+
+    public Todo(Long id, String title, String description){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Todo(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
